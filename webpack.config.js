b10db0e17b99a57b02492d7df3112edb037615de
Luca Bonora <bonora.luca@gmail.com>
2021-11-29 10:40:45 +0100

    Integrated our documentation viewer library (#2)
    * Import docViewer
    * Cleaned code
    
    Co-authored-by: Vincenzo1409 <vincenzo.armandi@hotmail.it>
    Co-authored-by: Domenico Rinaldo <domenico.rinaldo@sharedchains.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const ZipPlugin = require('zip-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -38,15 +39,23 @@ module.exports = {
   },
   devtool: 'cheap-module-source-map',
   plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, './style/style.css'),
+          from: path.resolve(__dirname, './node_modules/bpmn-js-documentation-viewer/assets/css/docViewer.css'),
           to: path.resolve(__dirname, './dist/')
         },
         {
           from: path.resolve(__dirname, './index.prod.js'),
           to: path.resolve(__dirname, './dist/index.js')
+        },
+        {
+          from: path.resolve(__dirname, './style/'),
+          to: path.resolve(__dirname, './dist/')
         }
       ]
     }),
@@ -54,8 +63,11 @@ module.exports = {
       filename: 'camunda-wysiwyg-documentation-plugin-' + process.env.npm_package_version + '.zip',
       pathPrefix: 'camunda-wysiwyg-documentation/',
       pathMapper: function(assetPath) {
-        if (assetPath.startsWith('client') || assetPath.startsWith('style')) {
+        if (assetPath.startsWith('client')) {
           return path.join(path.dirname(assetPath), 'client', path.basename(assetPath));
+        }
+        else if (assetPath.endsWith('.css')) {
+          return path.join(path.dirname(assetPath), 'style', path.basename(assetPath));
         }
         return assetPath;
       }
